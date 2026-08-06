4d06fa50dc007acaa77932e88cd8970c2da7a94c
bhavikamange1993@gmail.com <bhavikamange1993@gmail.com>
2026-08-06 22:18:53 +0000

    Save job functionality

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 from services.job_service import (
     fetch_remote_jobs,
     save_jobs_to_db,
     get_jobs
 )
+
+from database.lakebase import run_write
 
 
 jobs_bp = Blueprint(
@@ -26,3 +28,32 @@ def jobs():
         "jobs.html",
         jobs=saved_jobs
     )
+
+
+@jobs_bp.route("/save-job", methods=["POST"])
+def save_job():
+
+    job_id = request.form["job_id"]
+
+    sql = """
+    INSERT INTO saved_jobs
+    (
+        user_id,
+        job_id
+    )
+    VALUES
+    (
+        %s,
+        %s
+    )
+    """
+
+    run_write(
+        sql,
+        (
+            1,       # temporary user_id for Day 1 MVP
+            job_id
+        )
+    )
+
+    return redirect("/jobs")
